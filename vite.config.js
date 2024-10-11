1b509b6e0d419e125fbf77ffa188acef5e57cb88
Meshach <116318961+Meshach88@users.noreply.github.com>
2024-10-11 08:49:53 +0100

    Fix binding to 0.0.0.0 for Render deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',     // Bind to 0.0.0.0 to make the app accessible externally
+    port: process.env.PORT || 3000,
+  }
 })
